24f421bbe154c1c6dd0f9dd9862d7a3848cddc0b
DorianKossowski <doriancfc@gmail.com>
2021-03-18 22:34:26 +0100

    Add base for product-catalog service

---
+++ product-catalog-service/src/main/java/agh/fis/product_catalog/controller/ProductCatalogController.java
@@ -0,0 +1,17 @@
+package agh.fis.product_catalog.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductCatalogController {
+    private static final Logger logger = LoggerFactory.getLogger(ProductCatalogController.class);
+
+    @GetMapping("/")
+    public String hello() {
+        logger.info("Get on product catalog /");
+        return "Hello ProductCatalog";
+    }
+}
