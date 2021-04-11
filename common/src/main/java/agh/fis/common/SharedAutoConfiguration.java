f34795a654982d80c45381294cfebba7ca9d2fe3
DorianKossowski <doriancfc@gmail.com>
2021-04-11 18:01:17 +0200

    Create common module with shared properties

---
+++ common/src/main/java/agh/fis/common/SharedAutoConfiguration.java
@@ -0,0 +1,10 @@
+package agh.fis.common;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.util.ResourceUtils;
+
+@SpringBootApplication
+@PropertySource(ResourceUtils.CLASSPATH_URL_PREFIX + "application-shared.properties") //can be overridden by application.properties
+public class SharedAutoConfiguration {
+}
