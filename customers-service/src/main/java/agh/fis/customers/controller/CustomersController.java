08cc103f8caa0a27f56db5aceab9a62ab0e3ede9
DorianKossowski <doriancfc@gmail.com>
2021-04-13 20:38:41 +0200

    Create index endpoints for customers

--- customers-service/src/main/java/agh/fis/customers/controller/CustomersController.java
+++ customers-service/src/main/java/agh/fis/customers/controller/CustomersController.java
@@ -1,17 +1,32 @@
 package agh.fis.customers.controller;
 
+import agh.fis.customers.controller.api.CustomersApi;
+import agh.fis.customers.model.CustomerDto;
+import agh.fis.customers.model.CustomerRegistrationDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import java.util.List;
+
 @RestController
-public class CustomersController {
+public class CustomersController implements CustomersApi {
     private static final Logger logger = LoggerFactory.getLogger(CustomersController.class);
 
-    @GetMapping("/")
-    public String hello() {
-        logger.info("Get on customers /");
-        return "Hello Customers";
+    @Override
+    public ResponseEntity<CustomerDto> createCustomer(@Valid CustomerRegistrationDto customerRegistrationDto) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<CustomerDto>> getAllCustomers(String authorization) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<CustomerDto> updateCustomer(String authorization, @Valid CustomerDto customerDto) {
+        return null;
     }
 }
