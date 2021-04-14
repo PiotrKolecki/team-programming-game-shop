64e43066f1dc3b4f9ae35f39b73df7092cb8ec12
Szy11mon <szy111mon@wp.pl>
2021-04-14 19:16:28 +0200

    Shopping Cart API endpoints and dummy JSON

--- shopping-cart-service/src/main/java/agh/fis/shopping_cart/controller/ShoppingCartController.java
+++ shopping-cart-service/src/main/java/agh/fis/shopping_cart/controller/ShoppingCartController.java
@@ -3,7 +3,11 @@ package agh.fis.shopping_cart.controller;
 import agh.fis.shopping_cart.client.ProductCatalogClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.json.*;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,17 +21,53 @@ public class ShoppingCartController {
         this.productCatalogClient = productCatalogClient;
     }
 
-    @GetMapping("/")
-    public String hello() {
-        logger.info("Get on shopping cart /");
-        return "Hello ShoppingCart";
+    @GetMapping("/{id}")
+    public String getCart(@PathVariable String id) {
+        String message;
+        JSONObject json = new JSONObject();
+        json.put("id", 1);
+        json.put("id_customer", 3);
+        json.put("date_created", "20-03-2021");
+
+        JSONArray array = new JSONArray();
+        JSONObject item1 = new JSONObject();
+        item1.put("name", "The Witcher");
+        item1.put("id", 3);
+        item1.put("price", 80.5);
+        item1.put("publisher", "CD Projekt");
+        array.put(item1);
+
+        JSONObject item2 = new JSONObject();
+        item2.put("name", "GTA V");
+        item2.put("id", 4);
+        item2.put("price", 120);
+        item2.put("publisher", "Rockstar Games");
+        array.put(item2);
+
+        json.put("products", array);
+
+        message = json.toString();
+        return message;
     }
 
-    @GetMapping("/{id}")
-    public String test(@PathVariable String id) {
-        if(id.equals("1")) {
-            return productCatalogClient.test();
-        }
-        return "null";
+    @PostMapping("/")
+    public String createCart() {
+        logger.info("Get on shopping cart /");
+        return String.format("Cart with id %s created", 3);
     }
+
+
+    @PutMapping("/")
+    public String addOrRemove() {
+        logger.info("Get on shopping cart /");
+        return "Item added/removed";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable String id) {
+        logger.info("Get on shopping cart /");
+        return String.format("Cart of customer with id %s deleted", id);
+    }
+
+
 }
