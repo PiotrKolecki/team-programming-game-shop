547529105f9cfa9f2a3793ee08f2536551727b5a
awysocki <adrian.wyso.97@gmail.com>
2021-04-14 20:38:15 +0200

    Order managment API endpoints

--- order-management-service/src/main/java/agh/fis/order_management/controller/OrderManagementController.java
+++ order-management-service/src/main/java/agh/fis/order_management/controller/OrderManagementController.java
@@ -1,17 +1,49 @@
 package agh.fis.order_management.controller;
 
+import io.swagger.v3.core.util.Json;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
+//@RequestMapping("orders")
 public class OrderManagementController {
     private static final Logger logger = LoggerFactory.getLogger(OrderManagementController.class);
 
     @GetMapping("/")
-    public String hello() {
-        logger.info("Get on order management /");
-        return "Hello OrderManagement";
+    public String HandleGetOrders() {
+
+        return "[GET] Hello from orders/";
+    }
+
+    @GetMapping("/order/{id}")
+    public String HandleGetOrder(@PathVariable String id) {
+
+        return "[GET] Hello from orders/order/{id}";
+    }
+
+    @GetMapping("/customer/{id}")
+    public String HandleGetCustomerOrders(@PathVariable String id) {
+
+        return "[GET] Hello from orders/customer/{id}";
+    }
+
+    @PostMapping("/")
+    public String HandlePostOrder() {
+        return "[POST] Hello from orders/";
+    }
+
+    @PutMapping("/")
+    public String HandlePutOrder() {
+        return "[PUT] Hello from orders/";
+    }
+
+    @DeleteMapping("/{id}")
+    public String HandleDeleteOrder(@PathVariable String id) {
+
+        return "[DELETE] Hello from orders/{id}";
     }
 }
