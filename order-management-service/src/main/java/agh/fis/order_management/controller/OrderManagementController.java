045dbf63cc68c70cc7d74e7eb27285dec90f8fe8
awysocki <adrian.wyso.97@gmail.com>
2021-04-14 21:22:06 +0200

    Simple Json returned from get order/{id}

--- order-management-service/src/main/java/agh/fis/order_management/controller/OrderManagementController.java
+++ order-management-service/src/main/java/agh/fis/order_management/controller/OrderManagementController.java
@@ -1,6 +1,8 @@
 package agh.fis.order_management.controller;
 
 import io.swagger.v3.core.util.Json;
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -9,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
-//@RequestMapping("orders")
 public class OrderManagementController {
     private static final Logger logger = LoggerFactory.getLogger(OrderManagementController.class);
 
@@ -21,8 +22,31 @@ public class OrderManagementController {
 
     @GetMapping("/order/{id}")
     public String HandleGetOrder(@PathVariable String id) {
+        String message;
 
-        return "[GET] Hello from orders/order/{id}";
+        JSONObject response = new JSONObject();
+        response.put("id", 11);
+        response.put("id_customer", 3);
+        response.put("total_price", 199.99 + 149.99);
+        response.put("payment_id", 12345);
+
+        JSONObject g1 = new JSONObject();
+        g1.put("name", "Fifa 21");
+        g1.put("id", 30);
+        g1.put("price", 199.99);
+
+        JSONObject g2 = new JSONObject();
+        g2.put("name", "Game Name");
+        g2.put("id", 40);
+        g2.put("price", 149.99);
+
+        JSONArray products = new JSONArray();
+        products.add(g1);
+        products.add(g2);
+
+        response.put("products", products);
+
+        return response.toString();
     }
 
     @GetMapping("/customer/{id}")
