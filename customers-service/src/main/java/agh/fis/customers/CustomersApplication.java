38dc74d56fd49b2d400cca21579dbae6987e28b9
DorianKossowski <doriancfc@gmail.com>
2021-04-15 16:03:31 +0200

    Create customer in CustomerService

--- customers-service/src/main/java/agh/fis/customers/CustomersApplication.java
+++ customers-service/src/main/java/agh/fis/customers/CustomersApplication.java
@@ -1,10 +1,12 @@
 package agh.fis.customers;
 
 import agh.fis.common.SharedAutoConfiguration;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
@@ -15,5 +17,10 @@ public class CustomersApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CustomersApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper getModelMapper() {
+        return new ModelMapper();
     }
 }
---
+++ customers-service/src/main/java/agh/fis/customers/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package agh.fis.customers.repository;
+
+import agh.fis.customers.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+}
