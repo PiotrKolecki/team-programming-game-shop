03bb6d64a12a82a287fa5aad41a11e861110d149
awysocki <adrian.wyso.97@gmail.com>
2021-04-26 23:31:18 +0200

    [order-management] Created endpoints and DTOs with OpenAPI

---
+++ order-management-service/order-management-server/src/main/java/agh/fis/order_management/controller/OrderManagementController.java
@@ -0,0 +1,47 @@
+package agh.fis.order_management.controller;
+
+import agh.fis.order_management.controller.api.OrdersApi;
+import agh.fis.order_management.model.OrderDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+
+@RestController
+public class OrderManagementController implements OrdersApi {
+    private static final Logger logger = LoggerFactory.getLogger(OrderManagementController.class);
+
+    @Override
+    public ResponseEntity<OrderDto> createOrder(String authorization, @Valid OrderDto orderDto) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteOrderById(String authorization, Integer id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<OrderDto>> getCustomers(String authorization) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<OrderDto>> getUsersOrders(String authorization, Integer id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<OrderDto> idGet(String authorization, Integer id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<OrderDto> updateOrder(String authorization, @Valid OrderDto orderDto) {
+        return null;
+    }
+}
