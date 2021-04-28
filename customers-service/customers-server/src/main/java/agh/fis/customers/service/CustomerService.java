75def90d459412f9af57873efa595cdd5f1077ec
DorianKossowski <doriancfc@gmail.com>
2021-04-28 20:28:52 +0200

    Create shopping cart during user creation

--- customers-service/customers-server/src/main/java/agh/fis/customers/service/CustomerService.java
+++ customers-service/customers-server/src/main/java/agh/fis/customers/service/CustomerService.java
@@ -1,14 +1,17 @@
 package agh.fis.customers.service;
 
+import agh.fis.customers.client.ShoppingCartClient;
 import agh.fis.customers.model.Customer;
 import agh.fis.customers.model.CustomerDto;
 import agh.fis.customers.model.CustomerRegistrationDto;
+import agh.fis.customers.model.ShoppingCartCreationDto;
 import agh.fis.customers.repository.CustomerRepository;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
@@ -19,20 +22,32 @@ public class CustomerService {
 
     private final ModelMapper modelMapper;
     private final CustomerRepository repository;
+    private final ShoppingCartClient shoppingCartClient;
 
-    public CustomerService(ModelMapper modelMapper, CustomerRepository repository) {
+    public CustomerService(ModelMapper modelMapper, CustomerRepository repository, ShoppingCartClient shoppingCartClient) {
         this.modelMapper = modelMapper;
         this.repository = repository;
+        this.shoppingCartClient = shoppingCartClient;
     }
 
+    @Transactional
     public CustomerDto create(CustomerRegistrationDto customerRegistrationDto) {
         Customer rawCustomer = modelMapper.map(customerRegistrationDto, Customer.class);
         Customer customer = repository.save(rawCustomer);
 
-        // TODO create shopping cart
+        createCustomerShoppingCart(customer.getId());
 
         logger.info("Customer of type {} with id {} created", customer.getUserType(), customer.getId());
         return modelMapper.map(customer, CustomerDto.class);
+    }
+
+    void createCustomerShoppingCart(int customerId) {
+        try {
+            shoppingCartClient.createShoppingCart(new ShoppingCartCreationDto().customerId(customerId));
+        } catch (Exception e) {
+            logger.error("Exception during customer shopping cart creation:", e);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Unable to create customer shopping cart");
+        }
     }
 
     public CustomerDto getByMailAndPassword(String mail, String password) {
