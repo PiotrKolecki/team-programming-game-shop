a5e7de6441c4dfbaebe4c732f34d21198c587ee1
gravvy23 <majkutw@gmail.com>
2021-05-06 21:49:22 +0200

    add mobile menu

--- frontend/src/components/Masthead/Navigation.tsx
+++ frontend/src/components/Masthead/Navigation.tsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import classnames from "classnames";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
+import MoreIcon from "@material-ui/icons/MoreVert";
+import Menu, { MenuProps } from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 import { useLocation } from "react-router";
+import { theme as appTheme } from "../../constants";
 
 type NavItem = {
   name: string;
   href: string;
 };
+
+const StyledMenu = withStyles({
+  paper: {
+    background: appTheme.colors.tundora,
+  },
+})((props: MenuProps) => <Menu {...props} />);
 
 const useStyles = makeStyles((theme) => ({
   navigation: {
@@ -23,9 +34,21 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(1, 3),
     borderRadius: 0,
   },
-
   active: {
     borderBottom: "4px solid #4a50cb",
+  },
+  mobile: {
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none",
+    },
+  },
+  menuItem: {
+    textTransform: "uppercase",
+    "& a": {
+      textDecoration: "none",
+      color: "white",
+    },
   },
 }));
 
@@ -47,22 +70,65 @@ const navItems: Array<NavItem> = [
 export function Navigation() {
   const classes = useStyles();
   const { pathname } = useLocation();
+  const [
+    mobileMoreAnchorEl,
+    setMobileMoreAnchorEl,
+  ] = useState<null | HTMLElement>(null);
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const renderMobileMenu = (
+    <StyledMenu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      keepMounted
+      open={Boolean(mobileMoreAnchorEl)}
+      onClose={handleMobileMenuClose}
+    >
+      {navItems.map((item) => (
+        <MenuItem key={item.name} className={classes.menuItem}>
+          <a href={item.href}>{item.name}</a>
+        </MenuItem>
+      ))}
+    </StyledMenu>
+  );
 
   return (
-    <nav className={classes.navigation}>
-      {navItems.map((item) => (
+    <>
+      <nav className={classes.navigation}>
+        {navItems.map((item) => (
+          <Button
+            key={item.name}
+            size="large"
+            href={item.href}
+            className={classnames(
+              classes.button,
+              pathname === item.href && classes.active
+            )}
+          >
+            {item.name}
+          </Button>
+        ))}
+      </nav>
+      <nav className={classes.mobile}>
         <Button
-          key={item.name}
+          key="more"
           size="large"
-          href={item.href}
-          className={classnames(
-            classes.button,
-            pathname === item.href && classes.active
-          )}
+          aria-haspopup="true"
+          onClick={handleMobileMenuOpen}
+          className={classnames(classes.button)}
         >
-          {item.name}
+          <MoreIcon />
         </Button>
-      ))}
-    </nav>
+      </nav>
+      {renderMobileMenu}
+    </>
   );
 }
