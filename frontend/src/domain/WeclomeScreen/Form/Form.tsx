cbb913dce3fa207cf5f64863ba670a18415639c2
gravvy23 <majkutw@gmail.com>
2021-05-07 12:05:37 +0200

    add form validation

--- frontend/src/domain/WeclomeScreen/Form/Form.tsx
+++ frontend/src/domain/WeclomeScreen/Form/Form.tsx
@@ -1,17 +1,46 @@
 import React from "react";
+import classnames from "classnames";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import Divider from "@material-ui/core/Divider";
 import { Input } from "../../../components";
-import { Form as FinalForm, Field } from "react-final-form";
+import { Form as FinalForm, Field, FieldProps } from "react-final-form";
 import { theme as appTheme } from "../../../constants";
+
+type FooterProps = {
+  label: string;
+  title: string;
+  href: string;
+};
+
+type InputProps = Pick<FieldProps<any, any>, "validate"> & {
+  label: string;
+  inputProps?: React.ComponentProps<typeof Input>;
+};
+
+export type FormProps = {
+  submitTitle: string;
+  footer: FooterProps;
+  formElements: Array<InputProps>;
+};
 
 const useStyles = makeStyles((theme) => ({
   label: {
     color: "inherit",
     textTransform: "uppercase",
     fontSize: "1rem",
+  },
+  error: {
+    "& input": {
+      border: `1px solid ${appTheme.colors.guardsmanRed}`,
+    },
+  },
+  errorInfo: {
+    ...theme.typography.body2,
+    color: appTheme.colors.guardsmanRed,
+    position: "absolute",
+    right: 8,
   },
   submit: {
     color: "inherit",
@@ -40,23 +69,33 @@ const useStyles = makeStyles((theme) => ({
   newCustomer: {
     margin: theme.spacing(1, 1),
   },
+  gridItem: {
+    position: "relative",
+  },
 }));
 
-type FooterProps = {
-  label: string;
-  title: string;
-  href: string;
-};
-
-type InputProps = {
-  label: string;
-  inputProps?: React.ComponentProps<typeof Input>;
-};
-
-export type FormProps = {
-  submitTitle: string;
-  footer: FooterProps;
-  formElements: Array<InputProps>;
+const validator = (elements: Array<InputProps>) => (
+  values: Record<string, any>
+) => {
+  const errors: Record<string, string> = {};
+  elements.forEach(({ label }) => {
+    if (!values[label]) {
+      errors[label] = "Required";
+    } else if (
+      label === "email" &&
+      !/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        values[label].toLowerCase()
+      )
+    ) {
+      errors[label] = "Must be a mail";
+    } else if (
+      label === "repeat password" &&
+      values[label] !== values["password"]
+    ) {
+      errors[label] = "Must match";
+    }
+  });
+  return errors;
 };
 
 export function Form({ submitTitle, footer, formElements }: FormProps) {
@@ -64,44 +103,64 @@ export function Form({ submitTitle, footer, formElements }: FormProps) {
 
   return (
     <FinalForm
-      onSubmit={() => {}}
-      render={({ handleSubmit, pristine, submitting }) => (
-        <form onSubmit={handleSubmit}>
-          <Grid container spacing={2}>
-            {formElements.map(({ label, inputProps }) => (
-              <Grid item xs={12} sm={12} key={label}>
-                <Field name={label}>
-                  {({ input }) => (
-                    <>
-                      <label className={classes.label}>{label}</label>
-                      <Input {...input} {...inputProps} />
-                    </>
-                  )}
-                </Field>
-              </Grid>
-            ))}
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              disabled={submitting || pristine}
-              className={classes.submit}
-            >
-              {submitTitle}
-            </Button>
-            <Divider className={classes.divider} variant="fullWidth" />
-            <p className={classes.newCustomer}>{footer.title}</p>
-            <Button
-              fullWidth
-              href={footer.href}
-              variant="outlined"
-              className={classes.register}
-            >
-              {footer.label}
-            </Button>
-          </Grid>
-        </form>
-      )}
+      onSubmit={console.log}
+      validate={validator(formElements)}
+      render={({ handleSubmit, pristine, submitting, hasValidationErrors }) => {
+        return (
+          <form onSubmit={handleSubmit}>
+            <Grid container spacing={2}>
+              {formElements.map(({ label, inputProps, validate }) => (
+                <Grid
+                  item
+                  xs={12}
+                  sm={12}
+                  key={label}
+                  className={classes.gridItem}
+                >
+                  <Field name={label} validate={validate}>
+                    {({ input, meta }) => (
+                      <>
+                        <label className={classes.label}>{label}</label>
+                        {meta.touched && meta.error && (
+                          <span className={classes.errorInfo}>
+                            {meta.error}
+                          </span>
+                        )}
+                        <Input
+                          {...input}
+                          {...inputProps}
+                          className={classnames(
+                            meta.error && meta.touched && classes.error
+                          )}
+                        />
+                      </>
+                    )}
+                  </Field>
+                </Grid>
+              ))}
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                disabled={submitting || pristine || hasValidationErrors}
+                className={classes.submit}
+              >
+                {submitTitle}
+              </Button>
+              <Divider className={classes.divider} variant="fullWidth" />
+              <p className={classes.newCustomer}>{footer.title}</p>
+              <Button
+                fullWidth
+                href={footer.href}
+                variant="outlined"
+                className={classes.register}
+              >
+                {footer.label}
+              </Button>
+            </Grid>
+          </form>
+        );
+      }}
     />
   );
 }
--- frontend/src/domain/WeclomeScreen/utils.tsx
+++ frontend/src/domain/WeclomeScreen/utils.tsx
@@ -8,17 +8,17 @@ export type Props = {
   type: WelcomeType;
 };
 
-const signInFooter = {
+const SIGN_IN_FOOTER = {
   title: "Are you a new customer?",
   label: "register",
   href: "/register",
-};
+} as const;
 
-const registerFooter = {
+const REGISTER_FOOTER = {
   title: "Already have an account?",
   label: "log in",
   href: "/login",
-};
+} as const;
 
 const signInElements: FormProps["formElements"] = [
   {
@@ -58,7 +58,7 @@ const Purple = styled.div`
 export const mapTypeToFormProps = (type: WelcomeType): FormProps => ({
   submitTitle: type,
   formElements: type === "sign in" ? signInElements : registerElements,
-  footer: type === "sign in" ? signInFooter : registerFooter,
+  footer: type === "sign in" ? SIGN_IN_FOOTER : REGISTER_FOOTER,
 });
 
 export const mapTypeToTitle = (type: Props["type"]) => {
