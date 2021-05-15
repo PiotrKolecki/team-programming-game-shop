bd93411b0900ef8c81728eda2115aed14ef6892c
mrcmis <marcinxmis@gmail.com>
2021-05-15 15:08:30 +0200

    authentication/ add check token implementation

--- authentication-service/authentication-server/src/main/java/agh/fis/authentication/service/JwtTokenService.java
+++ authentication-service/authentication-server/src/main/java/agh/fis/authentication/service/JwtTokenService.java
@@ -1,7 +1,10 @@
 package agh.fis.authentication.service;
 
+import agh.fis.authentication.client.CustomerServiceClient;
+import agh.fis.authentication.model.AuthCustomerDto;
 import agh.fis.authentication.security.jwt.JwtGenerator;
 import agh.fis.authentication.security.jwt.JwtParser;
+import org.modelmapper.ModelMapper;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,21 +17,32 @@ public class JwtTokenService {
     private final JwtGenerator jwtGenerator;
     private final JwtParser jwtParser;
     private final UserDetailsService userDetailsService;
+    private final CustomerServiceClient customerServiceClient;
+    private final ModelMapper modelMapper;
 
-    public JwtTokenService(JwtGenerator jwtGenerator, JwtParser jwtParser, UserDetailsService userDetailsService) {
+    public JwtTokenService(JwtGenerator jwtGenerator, JwtParser jwtParser, UserDetailsService userDetailsService, CustomerServiceClient customerServiceClient, ModelMapper modelMapper) {
         this.jwtGenerator = jwtGenerator;
         this.jwtParser = jwtParser;
         this.userDetailsService = userDetailsService;
+        this.customerServiceClient = customerServiceClient;
+        this.modelMapper = modelMapper;
     }
 
     public String generateToken(Authentication authentication) {
         return jwtGenerator.createToken(authentication);
     }
 
-    public Optional<UserDetails> getUserFromToken(String token) {
+    public Optional<UserDetails> getUserDetailsFromToken(String token) {
         return Optional.ofNullable(token)
                 .map(jwtParser::retrieveSubject)
                 .map(userDetailsService::loadUserByUsername);
+    }
+
+    public Optional<AuthCustomerDto> getCustomerFromToken(String token) {
+        return Optional.ofNullable(token)
+                .map(jwtParser::retrieveSubject)
+                .map(customerServiceClient::getCustomerByMail)
+                .map(user -> modelMapper.map(user, AuthCustomerDto.class));
     }
 
     public boolean validateToken(String token) {
