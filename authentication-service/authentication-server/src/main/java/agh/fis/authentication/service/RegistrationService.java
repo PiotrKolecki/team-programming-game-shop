04ef4ff3aabcbfc0d66bcf3d36f0923fd649b46d
mrcmis <marcinxmis@gmail.com>
2021-05-29 13:09:59 +0200

    authentication/ hash passwords

--- authentication-service/authentication-server/src/main/java/agh/fis/authentication/service/RegistrationService.java
+++ authentication-service/authentication-server/src/main/java/agh/fis/authentication/service/RegistrationService.java
@@ -7,6 +7,7 @@ import agh.fis.customers.model.CustomerRegistrationDto;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -18,21 +19,31 @@ public class RegistrationService {
 
     private final CustomerServiceClient customerServiceClient;
     private final ModelMapper modelMapper;
+    private final PasswordEncoder passwordEncoder;
 
-    public RegistrationService(CustomerServiceClient customerServiceClient, ModelMapper modelMapper) {
+    public RegistrationService(CustomerServiceClient customerServiceClient,
+                               ModelMapper modelMapper,
+                               PasswordEncoder passwordEncoder) {
         this.customerServiceClient = customerServiceClient;
         this.modelMapper = modelMapper;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Optional<AuthCustomerDto> register(RegistrationDto registrationDto) {
         try {
             return Optional.of(registrationDto)
                     .map(registration -> modelMapper.map(registration, CustomerRegistrationDto.class))
+                    .map(this::hashPassword)
                     .map(customerServiceClient::register)
                     .map(customer -> modelMapper.map(customer, AuthCustomerDto.class));
         } catch (Exception e) {
             LOGGER.warn("Exception while user registration: " + e.getMessage());
             return Optional.empty();
         }
+    }
+
+    private CustomerRegistrationDto hashPassword(CustomerRegistrationDto registration) {
+        registration.setPassword(passwordEncoder.encode(registration.getPassword()));
+        return registration;
     }
 }
